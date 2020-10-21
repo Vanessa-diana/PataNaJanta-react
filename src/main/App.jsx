8c7989bf75da16b0f82431fb895f1c4d8a5fd36c
Joao Carlos de Oliveira <jcoliveiras23@gmail.com>
2020-10-21 18:10:46 -0300

    imagens

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -6,13 +6,12 @@ import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer';
-import Menu from '../components/Menu/Menu';
+
+
 
 export default props => 
     <div className="container-fluid">
         <Header/>
-        <Menu/>
-        <h1>Conteúdo</h1>
         <Footer/>
     </div>
 
