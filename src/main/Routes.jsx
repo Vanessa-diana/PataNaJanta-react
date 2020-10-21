e1a8fdc149621cd9f778bd716252d2367660bf0e
Lilian Milan <lilianmmilann@gmail.com>
2020-10-21 17:37:56 -0300

    sem router

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { Router,Route,Redirect,hashHistory } from 'react-router';
-import Home from '../pages/Home/Home'
-import Sucesso from '../pages/SucessoPedido/Sucesso'
-import Cadastro from '../pages/Cadastro/Cadastro'
+
+
 
 
 export default props => (
     <Router history={hashHistory}>
-    <Route path = '/home' component = {Home}/>
-    <Route path = '/sucesso' component = {Sucesso}/>
-    <Route path = '/cadastro' component = {Cadastro}/>
-    <Redirect from = '*' to ='/home'/>
+
     </Router>
 )
