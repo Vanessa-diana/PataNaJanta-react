6b33ec1d0afbf38f89ed8f94c601f1e126771946
Vanessa Diana <vanessa.d17@gmail.com>
2020-10-22 19:39:15 -0300

    correcao rotas

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -7,14 +7,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer';
 import Routes from './Routes'
-import resultado from '../pages/Resultado-produto/Resultado'
-import EnderecoCliente from '../pages/Endereco-cliente/EnderecoCliente';
+
 
 export default props => 
     <div className="container-fluid">
         <Header/>
         <Routes/>
         <Footer/>
-        <EnderecoCliente/>
     </div>
 
