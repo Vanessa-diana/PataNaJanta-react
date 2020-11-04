6dd3c4339718b844b777c97edeaa3c232634fc49
Thomaz Ferreira <thomaz.fneves@gmail.com>
2020-11-04 16:00:34 -0300

    esqueci minha senha OK

--- src/pages/EsqueciSenha/EsqueciSenha.jsx
+++ src/pages/EsqueciSenha/EsqueciSenha.jsx
@@ -1,9 +1,65 @@
 import React, { Component } from 'react'
 import '../EsqueciSenha/esquecisenha.css'
+import axios from 'axios'
 
 
 export default class EsqueciSenha extends Component{
 
+
+componentDidMount(){
+    this.novaSenha();
+}
+
+novaSenha = () => {
+
+    let btnEnviar = document.getElementById('btnEnviar');
+    let txtInput = document.getElementById('txtInput');
+    let self = this;
+
+    btnEnviar.addEventListener('click', function(event){
+
+        event.preventDefault();
+        txtInput.style.borderColor = '#ced4da';
+
+        this.disabled = true;
+        txtInput.disabled = true;
+
+        if(txtInput.value.length == 0){
+            alert('O campo indicado não pode estar vazio!')
+            txtInput.style.borderColor = 'red';
+
+            this.disabled = false;
+            txtInput.disabled = false;
+            return;
+        }
+
+        let URL = 'http://patanajanta.test/api';
+        let endPoint = `/usuario/recuperarsenha/${txtInput.value}`
+
+        URL+=endPoint;
+
+        axios.get(URL)
+        .then((resp)=>{
+            if(resp.data.status == 'sucesso'){
+                alert('Email enviado com sucesso! Por favor, verifique a caixa de entrada do e-mail vinculado a seu usuário.');
+                txtInput.value = '';
+            }
+            else{
+                alert(resp.data.status);
+                txtInput.value = '';
+            }
+
+            this.disabled = false;
+            txtInput.disabled = false;
+        })
+        .catch((erro)=>{
+            alert('ERRO AO ENVIAR EMAIL = ' + erro);
+
+            this.disabled = false;
+            txtInput.disabled = false;
+        })
+    });
+}
 
 render() {
 
@@ -13,12 +69,11 @@ render() {
                 <div className="form-group">
                     <h3 className= 'titulo-senha'>Resetar Senha</h3>
                     <label>Email ou CPF</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <input type="email" className="form-control" id="txtInput" aria-describedby="emailHelp"/>
                     <small className="form-text text-muted">Digite seu email ou CPF cadastrado.</small>
                 </div>
                 <div className="col text-center">
-                     <button type="submit" className="btn btn-padrao">Enviar</button>
-
+                     <button type="submit" className="btn btn-padrao" id='btnEnviar'>Enviar</button>
                 </div>
             </form>
         </div>
